30dfc0fc8895ed29c01057871f2f34ee6fc3a14a
Gabriel Azevedo <64609401+gabrielazevedo00@users.noreply.github.com>
2021-08-27 20:05:28 -0300

    Add files via upload

---
+++ Projeto.sql
@@ -0,0 +1,39 @@
+-- Selecionando 
+SELECT * FROM sales.transactions;
+
+select count(*) from sales.transactions;
+
+select count(*) from sales.customrs;
+
+-- Filtrando
+select * from sales.transactions where market_code="Mark001";
+
+select count(*) from sales.transactions where market_code="Mark001";
+
+select count(*) from sales.transactions where currency="USD";
+
+-- Inner Join
+
+select sales.transactions.*, sales.date.* 
+from sales.transactions 
+inner join sales.date
+on sales.transactions.order_date=sales.date.date 
+where sales.date.year=2020;
+
+-- Soma do total de vendas no ano de 2020
+select sum(sales.transactions.sales_amount) 
+from sales.transactions 
+inner join sales.date
+on sales.transactions.order_date=sales.date.date 
+where sales.date.year=2020;
+
+-- Filtro and 
+select sum(sales.transactions.sales_amount) 
+from sales.transactions 
+inner join sales.date
+on sales.transactions.order_date=sales.date.date 
+where sales.date.year=2020 and sales.transactions.market_code="Mark001";
+
+-- Filtro distinct
+select distinct product_code from sales.transactions where market_code='Mark001';
+
